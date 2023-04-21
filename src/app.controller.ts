93bb9f82d3e53d1164ed16204107405eff876628
carls1720 <quintero.carlos.rocha@gmail.com>
2023-04-20 22:00:13 -0500

    parametros y query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+//endpoint de bienvenida
   @Get()
   getInicio(): string{
     return this.appService.getInicio();
   }
-  
+  //endpoint de name
+  @Get('name')
+  getName(): String{
+    return this.appService.getName();
+  }
+//obtener el parametro incio y captura el nombre
+  @Get('inicio/:name')
+  getInicioByName(@Param('name')name: string): string{
+    return this.appService.getInicioByName(name);
+  }
+//obtencion de parametros tipo query, en este caso elementos
+ @Get('elements')
+ getElements(@Query('limit')limit= 20, @Query('offset')offset= 0):string{
+  return this.appService.getElements(limit, offset);
+ }
+
   @Get(':edad')
   getEdadActual(@Param('edad') edad: number): string{
     // convertimos el parametro a tipo numero para poder hacer operaciones ya que del link viene tipo string
     return this.appService.getEdad(Number(edad));
   }
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,11 +4,25 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   
   getInicio(): string{
-    return 'Bienvenido a la app';
+    return 'Bienvenido a la app de Nest.js, usted se encuentra en la página principal';
   }
 
   getEdad(edad): string {
     let fecha = new Date();
-    return 'Saludos usted tiene: ' + (fecha.getFullYear() - edad)
+    return 'Saludos usted tiene: ' + (fecha.getFullYear() - edad +  ' años ')
   }
+ 
+  getName(): string{
+    return 'Mi nombre es Nest'
+  }
+  getElements(limit: number, offset: number): string{
+    return `productos desde el id ${offset} hasta el id ${limit}`;
+  }
+  getInicioByName(name:string): string{
+    return this.getInicio() + ' ' + '' + name;
+  }
+  
+ 
+
+
 }
